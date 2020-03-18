2723a11e5480185eecfb821f73fe270681dc31cd
Shawn Tian <hightian@gmail.com>
2020-03-18 22:00:40 +0800

    Update 002_copy_file.py

--- 002_copy_file.py
+++ 002_copy_file.py
@@ -1,2 +1,14 @@
+"""
+┌──────────────────┬────────┬───────────┬───────┬────────────────┐
+│     Function     │ Copies │   Copies  │Can use│   Destination  │
+│                  │metadata│permissions│buffer │may be directory│
+├──────────────────┼────────┼───────────┼───────┼────────────────┤
+│shutil.copy       │   No   │    Yes    │   No  │      Yes       │
+│shutil.copyfile   │   No   │     No    │   No  │       No       │
+│shutil.copy2      │  Yes   │    Yes    │   No  │      Yes       │
+│shutil.copyfileobj│   No   │     No    │  Yes  │       No       │
+└──────────────────┴────────┴───────────┴───────┴────────────────┘
+"""
+
 from shutil import copyfile
 copyfile(src, dst)
